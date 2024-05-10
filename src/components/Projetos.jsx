b6d02872d9bdeeb00a80db4d5c61004c803cc255
Paulo Henrique <ph55@hotmail.com>
2024-05-10 09:37:53 -0300

    Update Projetos.jsx

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -7,17 +7,17 @@ function Projetos(){
         <section className='projetos-container'>
             <div className='projetos-box'>
                 <div className='projeto-titulo'>Estatísticas Campeonato Brasileiro 2023</div>
-                <a href="https://app.powerbi.com/reportEmbed?reportId=2a43f466-1809-45e5-9dd5-dfe56205c263&autoAuth=true&ctid=bf9bde85-68a4-4d6e-954b-544a50edb581" target='_blank'><img className='img-tablet' src={iconTablet01} alt="icon tablet" /></a>
+                <a href="https://app.powerbi.com/view?r=eyJrIjoiOTlkNTEzZDktYzc4Ny00NmNmLWI1YzUtMzM0MjczMGFiNDYzIiwidCI6ImJmOWJkZTg1LTY4YTQtNGQ2ZS05NTRiLTU0NGE1MGVkYjU4MSJ9" target='_blank'><img className='img-tablet' src={iconTablet01} alt="icon tablet" /></a>
                 <div className='box-links'>
-                    <a href="https://app.powerbi.com/reportEmbed?reportId=2a43f466-1809-45e5-9dd5-dfe56205c263&autoAuth=true&ctid=bf9bde85-68a4-4d6e-954b-544a50edb581" target='_blank'>Acessar o Relátorio</a>
+                    <a href="https://app.powerbi.com/view?r=eyJrIjoiOTlkNTEzZDktYzc4Ny00NmNmLWI1YzUtMzM0MjczMGFiNDYzIiwidCI6ImJmOWJkZTg1LTY4YTQtNGQ2ZS05NTRiLTU0NGE1MGVkYjU4MSJ9" target='_blank'>Acessar o Relátorio</a>
                 </div>
             </div>
 
             <div className='projetos-box box002'>
                 <div className='projeto-titulo'>Relátorio de Vendas - Tech Wave</div>
-                <a href="https://app.powerbi.com/reportEmbed?reportId=2fca866b-0946-4c7a-9d77-eb856be4216f&autoAuth=true&ctid=bf9bde85-68a4-4d6e-954b-544a50edb581" target='_blank'><img className='img-tablet' src={iconTablet02} alt="icon tablet" /></a>
+                <a href="https://app.powerbi.com/view?r=eyJrIjoiZmQ0MjkzMzYtNTkwZS00MWEyLWJmNjktZDNjNzMyOWMxZmY1IiwidCI6ImJmOWJkZTg1LTY4YTQtNGQ2ZS05NTRiLTU0NGE1MGVkYjU4MSJ9" target='_blank'><img className='img-tablet' src={iconTablet02} alt="icon tablet" /></a>
                 <div className='box-links'>
-                    <a href="https://app.powerbi.com/reportEmbed?reportId=2fca866b-0946-4c7a-9d77-eb856be4216f&autoAuth=true&ctid=bf9bde85-68a4-4d6e-954b-544a50edb581" target='_blank'>Acessar Relátorio</a>
+                    <a href="https://app.powerbi.com/view?r=eyJrIjoiZmQ0MjkzMzYtNTkwZS00MWEyLWJmNjktZDNjNzMyOWMxZmY1IiwidCI6ImJmOWJkZTg1LTY4YTQtNGQ2ZS05NTRiLTU0NGE1MGVkYjU4MSJ9" target='_blank'>Acessar Relátorio</a>
                 </div>
             </div>
         </section>
